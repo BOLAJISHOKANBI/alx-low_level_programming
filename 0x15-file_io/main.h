4199d8b0753a7756161a58d9db72c4524cf63178
BOLAJI <ibrahimshokanbi98@gmail.com>
2022-10-31 23:09:47 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,11 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-/* open */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-/* close */
 #include <unistd.h>
-
-/* dprintf */
 #include <stdio.h>
-
-/* malloc, free */
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
